fb8ece7c7b161033d62c8fa9df71978e9e1eb5c0
adselvar <adhita94@gmail.com>
2019-11-19 15:19:31 -0800

    add local files to gitignore

--- README.md
+++ README.md
@@ -6,4 +6,9 @@ Kubeflow Scanner is a tool to walk through all images in the kubeflow-images-pub
 
 ## Quick Start
 
+```bash
+export IMAGE_FILE_NAME=images.txt
+export GCP_PROJECT=<YOUR_GCP_PROJECT>
+```
+
 `make all`
--- list-images.sh
+++ list-images.sh
@@ -1,4 +1,4 @@
 GO111MODULE=on go get -u github.com/google/go-containerregistry/cmd/gcrane
 #gcrane cp -r gcr.io/kubeflow-images-public gcr.io/{GCP_PROJECT}
-# remove the grep command to list all images in the repo to the images.txt
-gcrane ls -r gcr.io/${GCP_PROJECT} | grep tensorflow-1.14.0 > images.txt
+# remove the grep command to list all images in the repo to the image file
+gcrane ls -r gcr.io/${GCP_PROJECT} | grep tensorflow-1.14.0 > ${IMAGE_FILE_NAME}
